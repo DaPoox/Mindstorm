98485b848a61a609fb24de3a7cea6bf1d00d9a94
Kerris Abderrahmane <abderrahmane.kerris@gmail.com>
2015-10-06 16:08:16 +0200

    ajout du premier programme Hello World + mise à jour fichier Readme

---
+++ HelloWorld/src/MainClass.java
@@ -0,0 +1,8 @@
+import lejos.nxt.Button;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("Hello world from bibliotheque\n");
+		Button.waitForAnyPress();
+	}
+}
