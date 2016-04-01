32f478e64563dcbacbbe4c9438095d46e8b0d390
kerris <kerris@informatique.univ-paris-diderot.fr>
2016-04-01 15:47:56 +0200

    Detection du type du mouvement du robot, 1ere version.

--- LineFollower2/src/Mouvement.java
+++ LineFollower2/src/Mouvement.java
@@ -20,6 +20,8 @@ public class Mouvement {
 	DifferentialPilot pilot;
 	Navigator navigator;
 	PoseProvider pp;
+
+	ArrayList<Point> listPoints;
 	
 	double lumBord;
 	double range;
@@ -37,6 +39,7 @@ public class Mouvement {
 
 		navigator = new Navigator(pilot);
 		pp = new OdometryPoseProvider(pilot);
+		this.listPoints = new ArrayList<Point>();
 	}
 	
 	public void sortirDebut(Couleur couleurDebut, Couleur couleurLigne){
@@ -60,7 +63,6 @@ public class Mouvement {
 		Motor.C.setSpeed(defaultSpeed/2);
 		
 		long t= System.currentTimeMillis();
-		//while(Motor.A.getTachoCount() < 200){
 		while(System.currentTimeMillis()<t+1000){	
 			Motor.A.forward();
 			Motor.C.backward();
@@ -76,7 +78,6 @@ public class Mouvement {
 	
 		this.lumBord = (lumMax+lumMin)/2;
 		
-		//Button.waitForAnyPress();
 		while(cs.cs.getLightValue() > lumBord){
 			Motor.C.forward();
 			Motor.A.backward();
@@ -84,20 +85,20 @@ public class Mouvement {
 		Delay.msDelay(150);
 		Motor.A.stop();
 		Motor.C.stop();
-		LCD.clear();
-		LCD.drawString("Ready ..."+lumBord, 0, 0);
-	//	Button.waitForAnyPress();
-		LCD.clear();
+		
 		this.range = lumMax - lumMin;
 	}
+	
 	public Path avancer(Couleur couleurLigne, Couleur couleurFin){	
 
 		double lumCouleurLu;
 		Point location;
 		long t1, t2;
+
+		location = pp.getPose().getLocation();
+		listPoints.add(location);
 		
 		pilot.forward();
-		ArrayList<Point> listPoints = new ArrayList<Point>();
 	
 		LCD.clear();
 
@@ -109,34 +110,32 @@ public class Mouvement {
 		
 			if(lumCouleurLu>lumBord){
 				acceleration *= 2;	
-			}
-			
+			}		
 			pilot.steer(acceleration);
+			
+			//Get la position chaque seconde
 			t2 = System.currentTimeMillis();
-			if((t2-t1)>1000){
+			if((t2-t1)>2000){
 				location = pp.getPose().getLocation();
+				//Afficher le type du deplacement
+				this.getMouvementType(location);
 				listPoints.add(location);
-				LCD.clear();
 				t1 = System.currentTimeMillis();
 			}
-			
 		}
-		
 		//On a terminé la ligne, on sauvgarde les points dans Path: 
 		pilot.stop();
 		
 		Path path = new Path();
 		for(Point point : listPoints){
 			LCD.clear();
-			System.out.print("X= "+point.getX());
-			System.out.print("\nY= "+point.getY());
 			Waypoint waypoint = new Waypoint(point.getX(), point.getY());
 			path.add(waypoint);
 		}
 		LCD.clear();
 		return path;
 	}
-	
+
 	public void demiTour(Couleur couleurLigne){
 		Motor.A.setSpeed(defaultSpeed/2);
 		Motor.C.setSpeed(defaultSpeed/2);
@@ -153,7 +152,28 @@ public class Mouvement {
 		Motor.A.stop();
 		Motor.C.stop();
 	}
+	
+	public void getMouvementType(Point location){
+		Point last = this.listPoints.get(this.listPoints.size()-1);
+		double dif = 2;
+		if(location.getY() < last.getY() + dif && location.getY() > last.getY() - dif){
+			LCD.clear();
+			LCD.drawString("Robot tout droit", 0, 1);
+		}else if(location.getY() > last.getY() + dif){
+			LCD.clear();
+			LCD.drawString("Virage gauche", 0, 1);;
+		}else{
+			LCD.clear();
+			LCD.drawString("Virage à droite",0,1);
+		}
+		LCD.drawString("X:"+location.getX(), 0,2);
+		LCD.drawString("Y:"+location.getY(), 0,3);
 
+		pilot.stop();
+		Button.waitForAnyPress();
+		pilot.forward();
+	}
+	
 	public void stop(){
 		Motor.A.stop();
 		Motor.C.stop();
@@ -163,6 +183,8 @@ public class Mouvement {
 	public void follow(Path path){
 		this.navigator.followPath(path);
 	}
+	
+
 }
 
 
