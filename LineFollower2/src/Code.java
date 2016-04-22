de7633eb5b325009594c295ca1811a09ef3e2b41
tounsi <tounsi.abdelkader.hotmail.com>
2016-04-22 15:46:44 +0200

    Remplacement de Color par Couleur

--- LineFollower2/src/Code.java
+++ LineFollower2/src/Code.java
@@ -2,37 +2,37 @@ import lejos.nxt.LCD;
 import lejos.nxt.ColorSensor.Color;
 
 public class Code {
-	private Color c1;
-	private Color c3;
-	private Color c2;
+	private Couleur c1;
+	private Couleur c3;
+	private Couleur c2;
 	
-	public Code(Color c1, Color c2, Color c3){
+	public Code(Couleur c1, Couleur c2, Couleur c3){
 		this.c1 = c1;
 		this.c2 = c2;
 		this.c3 = c3;
 	}
 	
-	public Color getC1() {
+	public Couleur getC1() {
 		return c1;
 	}
 
-	public void setC1(Color c1) {
+	public void setC1(Couleur c1) {
 		this.c1 = c1;
 	}
 
-	public Color getC2() {
+	public Couleur getC2() {
 		return c2;
 	}
 
-	public void setC2(Color c2) {
+	public void setC2(Couleur c2) {
 		this.c2 = c2;
 	}
 
-	public Color getC3() {
+	public Couleur getC3() {
 		return c3;
 	}
 
-	public void setC3(Color c3) {
+	public void setC3(Couleur c3) {
 		this.c3 = c3;
 	}
 
